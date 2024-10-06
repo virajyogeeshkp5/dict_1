35bde47e4ccb7c8a0ce915602d676ad355869325
YOGEESH K P <yogeesh793@gmail.com>
2024-10-06 13:04:57 +0530

    Add files via upload

---
+++ dict_1.py
@@ -0,0 +1,6 @@
+# Dictionary this is execution for single value
+dict_val = {"vani":9999,"santhi":999,"nandhi":885}
+print(dict_val["santhi"])
+print(type(dict_val))
+print(dict_val)
+print(dict_val["vani"])
